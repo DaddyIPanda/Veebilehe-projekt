73c97978f6e8b716faea80520581034a08ceee5c
Maria Elisa Vassiljeva <mariaelisa@ut.ee>
2025-12-03 00:07:43 +0200

    valideerisin koodi ja parandasin vigu

--- assets/javascript/galerii-nuppud.js
+++ assets/javascript/galerii-nuppud.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Initsialiseeri slider kui DOM on laaditud
     initSlider();
 });
